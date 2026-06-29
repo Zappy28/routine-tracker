9086afa14a2082320ccbbe683b8dce9cd1c677a0
Zappy28 <jacobmichaelmo@outlook.com>
2026-06-29 15:38:48 -0600

    Added Firebase hosting setup

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDcMInWptZfDOEiAgX71fiWgzHtdjJS3a0",
+  authDomain: "routine-tracker-82e5c.firebaseapp.com",
+  projectId: "routine-tracker-82e5c",
+  storageBucket: "routine-tracker-82e5c.firebasestorage.app",
+  messagingSenderId: "769942353616",
+  appId: "1:769942353616:web:1f85abf21745b58e3010d5",
+  measurementId: "G-17TFS0024E"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
